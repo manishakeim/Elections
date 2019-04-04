b3e0010814350380c5bc10906869019eebd31dcf
Manisha Keim <manisha.keim@gmail.com>
2019-04-04 23:48:57 +0530

    Initial commit

---
+++ app/src/main/java/com/example/manisha/election/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.manisha.election;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+//        EditText t = (EditText) findViewById(R.id.editText_review);
+//        t.setGravity(Gravity.CENTER);
+    }
+}
